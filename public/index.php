5ff836008a8d74550c00cef2f33d74a1636ed062
andsaru <andsaru@hotmail.es>
2021-06-08 12:47:36 +0200

    mostrar por pantalla employees

--- public/index.php
+++ public/index.php
@@ -1,18 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?php require 'partial/head.php'; ?>
 
 <body>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/partial/header.php'; ?>
     <?php
-        include("./link.php") // include para añadir el enlace que está en link.php
-    ?>
-
-    <?php $weekDays = [
+    $weeekDays = [
         1 => 'lunes',
         2 => 'martes',
         3 => 'miércoles',
@@ -22,21 +15,17 @@
         7 => 'domingo',
     ];
     ?>
-
-    <?php
-       // var_dump($_GET); // para verlo solo por pantalla
-    ?>
-
-    <!-- <pre>
-        &_GET: <?php var_dump($_GET); ?>
+    <!-- pre>
+        $_GET: <?php //var_dump($_GET); ?>
         <br/>
-        &_POST: <?php //print_r($_POST); ?>
+        $_POST: <?php //print_r($_POST); ?>
         <br/>
-        &_SERVER: <?php //print_r($_SERVER; ?>
+        $_SERVER: <?php //print_r($_SERVER); ?>
         <br/>
-    </pre> -->
-
-    <h1>Hola, <?= $_GET['token']; ?></h1>
-    <p>Hoy es <strong><?= $weekDays[date('N')]; ?></strong> ,¿Qué tal estás?</p>
+    </pre -->
+    <h1>Hola, <?= $_GET['name']; ?></h1>
+    <p>Hoy es <strong><?= $weeekDays[date('N')]; ?></strong>. ¿Qué tal estás?</p>
+    <?php require 'partial/footer.php'; ?>
 </body>
+
 </html>
